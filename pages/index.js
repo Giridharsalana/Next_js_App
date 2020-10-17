5e3e5d0a8938cbfcb622dd57d75b4531df2f7740
GiridharSalana <GiridharSalana@gmail.com>
2020-10-17 19:01:43 +0530

    Updates

--- pages/index.js
+++ pages/index.js
@@ -1,48 +1,49 @@
 import Head from 'next/head'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>Giridhar Salana</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        Giridhar <Link href="/posts/first-post"><a>Salana</a></Link>
         </h1>
 
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+           <code>My blog With React js</code>
         </p>
 
         <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+          <a href="/posts/first-post" className="card">
+            <h3>Work</h3>
+            <p>Electronics Engineer.</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+          <a href="/posts/first-post" className="card">
+            <h3>Education</h3>
+            <p>Engineering.</p>
           </a>
 
           <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            href="/posts/first-post"
             className="card"
           >
-            <h3>Deploy &rarr;</h3>
+            <h3>Loves</h3>
+            <p>Exploring.</p>
+          </a>
+
+          <a
+            href="/posts/first-post"
+            className="card"
+          >
+            <h3>Deploy</h3>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              Instantly.
             </p>
           </a>
         </div>
@@ -152,7 +153,7 @@ export default function Home() {
           margin: 1rem;
           flex-basis: 45%;
           padding: 1.5rem;
-          text-align: left;
+          text-align: center;
           color: inherit;
           text-decoration: none;
           border: 1px solid #eaeaea;
@@ -188,7 +189,7 @@ export default function Home() {
             flex-direction: column;
           }
         }
-      `}</style>
+        `}</style>
 
       <style jsx global>{`
         html,
